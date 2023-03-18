bc6bb121a30efaf894812fc07f7ea06fa0cade65
dgehret <84560332+dgehret@users.noreply.github.com>
2023-03-18 14:16:40 -0700

    Added multiple features to each file

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
+const parallax = document.querySelector('.parallax');
+
+window.addEventListener('scroll', function() {
+  const yOffset = window.pageYOffset;
+  const parallaxOffset = yOffset * -0.5;
+
+  parallax.style.backgroundPosition = `center ${parallaxOffset}px`;
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
     const hamburgerMenu = document.querySelector(".hamburger-menu");
     const sidePanel = document.querySelector(".side-panel");
 
-    hamburgerMenu.addEventListener("click", function() {
+    hamburgerMenu.addEventListener("click", function () {
         sidePanel.classList.toggle("visible");
     });
-}); 
+});
+
+window.addEventListener('click', function (event) {
+    const sidePanel = document.querySelector('.side-panel');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+    if (event.target !== sidePanel && event.target !== hamburgerMenu && !sidePanel.contains(event.target)) {
+        sidePanel.classList.remove('visible');
+    }
+});
